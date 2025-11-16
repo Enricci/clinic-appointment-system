8e3701ae54e133066df6e9a8cc0325fbf4a87485
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2025-11-16 19:45:47 +0800

    Add initial URL routing, views, and templates for the clinic appointment system. Implement home view and base layout with navigation.

--- clinic_project/clinic_app/views.py
+++ clinic_project/clinic_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    """Home page view"""
+    context = {
+        'title': 'Home',
+        'page_title': 'Welcome to Clinic Appointment System'
+    }
+    return render(request, 'clinic_app/home.html', context)
