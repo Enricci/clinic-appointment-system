8690785f485d8b7fe98c468a119552cd992f4cbd
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2025-11-19 19:55:25 +0800

    Update startup script: remove CSS watcher and change server address to 127.0.0.1

--- startup.sh
+++ startup.sh
@@ -1,10 +1,7 @@
-# Clinic Appointment System - Startup Script
+# Startup Script
 
 pipenv run python clinic_project/manage.py runserver
-npm run watch:css
-
-echo "Server started at http://localhost:8000"
-echo "CSS watcher started"
+echo "Server started at http://127.0.0.1:8000"
 
 # Keep the script running to prevent the container from exiting
 while true; do
