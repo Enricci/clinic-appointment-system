5e7bbf22eecf7f74fb308e91bdd415bbd397ddf0
Darl Ellison Floresca <floresca.darlellisong@gmail.com>
2025-11-19 20:24:39 +0800

    Add admin interfaces for Doctor, Patient, Appointment, and Prescription models. Implement list display, search fields, and filters

--- clinic_project/clinic_app/admin.py
+++ clinic_project/clinic_app/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Doctor, Patient, Appointment, Prescription
 
-# Register your models here.
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'specialization', 'phone_number')
+    search_fields = ('user__first_name', 'user__last_name', 'specialization')
+    list_filter = ('specialization',)
+
+
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_of_birth', 'email', 'phone_number')
+    search_fields = ('user__first_name', 'user__last_name', 'email', 'phone_number')
+    list_filter = ('date_of_birth',)
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'doctor', 'appointment_date', 'appointment_time', 'status')
+    search_fields = ('patient__user__first_name', 'patient__user__last_name', 'doctor__user__last_name')
+    list_filter = ('status', 'appointment_date', 'doctor__specialization')
+    date_hierarchy = 'appointment_date'
+    ordering = ('-appointment_date',)
+
+
+@admin.register(Prescription)
+class PrescriptionAdmin(admin.ModelAdmin):
+    list_display = ('appointment', 'medication', 'get_patient', 'get_doctor')
+    search_fields = ('medication', 'appointment__patient__user__first_name', 'appointment__patient__user__last_name')
+    
+    def get_patient(self, obj):
+        return obj.appointment.patient
+    get_patient.short_description = 'Patient'
+    
+    def get_doctor(self, obj):
+        return obj.appointment.doctor
+    get_doctor.short_description = 'Doctor'
